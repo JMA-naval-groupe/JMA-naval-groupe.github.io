ce309024924908d4211151098cae9e45b904e99a
JMA-naval-groupe <maximej836@gmail.com>
2026-03-05 08:31:18 +0100

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,122 @@
+var q=["https://www.twitch.tv/smegmaj/about","https://www.instagram.com/maxime_chill/",""]
+var d
+var bat6=["https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084311311-1_XLARGE.jpg?w=1028&ratio=default&t=1741794192000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084306504-1_XLARGE.jpg?w=1028&ratio=default&t=1741794187000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084319322-1_XLARGE.jpg?w=1028&ratio=default&t=1741794200000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084315368-1_XLARGE.jpg?w=1028&ratio=default&t=1741794196000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084320325-1_XLARGE.jpg?w=1028&ratio=default&t=1741794201000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084300273-1_XLARGE.jpg?w=1028&ratio=default&t=1741794181000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084308287-1_XLARGE.jpg?w=1028&ratio=default&t=1741794189000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084310300-1_XLARGE.jpg?w=1028&ratio=default&t=1741794191000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084329340-1_XLARGE.jpg?w=1028&ratio=default&t=1741794210000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084326351-1_XLARGE.jpg?w=1028&ratio=default&t=1741794207000&exact&format=webp","https://images.boatsgroup.com/resize/1/29/23/2016-sanlorenzo-sl86-power-9712923-20250312084254293-1_XLARGE.jpg?w=1028&ratio=default&t=1741794175000&exact&format=webp"]
+var bat5=["https://media.gqmagazine.fr/photos/5d385508a556b70008474873/16:9/w_1920,c_limit/M-Y-Adastra-Carbon-Yacht-2.jpg","https://media.gqmagazine.fr/photos/5d385e5aa556b7000847487b/master/w_1600,c_limit/M-Y-Adastra-Carbon-Yacht-0-Hero.jpg","https://media.gqmagazine.fr/photos/5d38555dd315b9000997f2be/master/w_1600,c_limit/article-0-1A448387000005DC-228_964x589.jpg","https://media.gqmagazine.fr/photos/5d385e83d315b9000997f2ca/master/w_1600,c_limit/M-Y-Adastra-Carbon-Yacht-1.jpg","https://media.gqmagazine.fr/photos/5d385576d315b9000997f2c0/master/w_1600,c_limit/article-2341061-1A4482A3000005DC-823_964x644.jpg","https://media.gqmagazine.fr/photos/5d385ee5d315b9000997f2cc/master/w_1600,c_limit/M-Y-Adastra-Carbon-Yacht-3.jpg","https://media.gqmagazine.fr/photos/5d38558fa556b70008474877/master/w_1600,c_limit/M-Y-Adastra-Carbon-Yacht-4.jpg","https://media.gqmagazine.fr/photos/5d385cd6a556b70008474879/master/w_1600,c_limit/M-Y-Adastra-Carbon-Yacht-5.jpg"]
+var bat4=["https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623085534486-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623085513630-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623085519557-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623085523592-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623085528267-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623085532977-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623073650303-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623085511319-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623085517004-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623073654005-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623073703306-1.jpg?w=969&ratio=default&t=1750695035000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623073742116-1.jpg?w=969&ratio=default&t=1750695036000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623073748346-1.jpg?w=969&ratio=default&t=1750695036000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623073813082-1.jpg?w=969&ratio=default&t=1750695036000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623073818958-1.jpg?w=969&ratio=default&t=1750695036000&exact&format=webp","https://images.boatsgroup.com/resize/1/89/33/2023-sunseeker-100-yacht-power-9848933-20250623073835425-1.jpg?w=969&ratio=default&t=1750695036000&exact&format=webp"]
+var bat3=["https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025902668_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021758310_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021759069_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025900167_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021912413_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021801218_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021911666_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021908808_1_XLARGE.jpg?w=1028&ratio=default&t=1580465949000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025856229_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021910988_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025850399_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021910313_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025852208_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021909610_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021909610_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025858698_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025854512_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025848513_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021759752_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20200131021800442_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp","https://images.boatsgroup.com/resize/1/27/15/7352715_20210517025904507_1_XLARGE.jpg?w=1028&ratio=default&t=1621245916000&exact&format=webp"]
+var bat2=["https://cdn2.riv-art.fr/img/annonce/26799/1-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/2-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/3-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/4-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/5-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/6-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/7-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/8-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/9-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/10-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/11-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/12-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/13-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/14-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/15-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/16-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/17-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/18-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/19-ladenstein72fly.webp","https://cdn2.riv-art.fr/img/annonce/26799/20-ladenstein72fly.webp"]
+var bat1=["https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205183955796-1_XLARGE.jpg?t=1725421354000&w=600&h=600","https://images.boatsgroup.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205185245900-1_XLARGE.jpg?w=969&ratio=default&t=1733453748000&exact&format=webp","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205183825547-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205183955796-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205183840188-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205183853801-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184712434-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184850607-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205183923287-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184805600-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205183909135-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184819252-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184835010-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205183936878-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184903337-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184627055-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184643355-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184658872-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20241205184728496-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200","https://images.boats.com/resize/1/10/55/2024-riva-110-dolcevita-power-9531055-20240903221507266-1_XLARGE.jpg?t=1725421354000&w=1200&h=1200"]
+var b=0
+document.getElementById("bateau7").hidden=true;
+function changebat1() {
+	b=(b+1)%bat1.length
+	document.getElementById('bat').src=bat1[b]
+	/* pour aller vers la droite */
+}
+
+function q() {
+	if (b==0) {b=q.length-1}
+	else{b=(b-1)%q.length}
+	document.getElementById('bat').src=q[b]
+}
+
+
+function changesbat1() {
+	if (b==0) {b=bat1.length-1}
+	else{b=(b-1)%bat1.length}
+	document.getElementById('bat').src=bat1[b]
+	/*pour aller vers la gauche*/
+}
+
+function changebat2() {
+	b=(b+1)%bat2.length
+	document.getElementById('bat').src=bat2[b]
+	/* pour aller vers la droite */
+}
+function changesbat2() {
+	if (b==0) {b=bat2.length-1}
+	else{b=(b-1)%bat2.length}
+	document.getElementById('bat').src=bat2[b]
+	/*pour aller vers la gauche*/
+}
+
+function changebat3() {
+	b=(b+1)%bat3.length
+	document.getElementById('bat').src=bat3[b]
+	/* pour aller vers la droite */
+}
+function changesbat3() {
+	if (b==0) {b=bat3.length-1}
+	else{b=(b-1)%bat3.length}
+	document.getElementById('bat').src=bat3[b]
+	/*pour aller vers la gauche*/
+}
+
+function changebat4() {
+	b=(b+1)%bat4.length
+	document.getElementById('bat').src=bat4[b]
+	/* pour aller vers la droite */
+}
+function changesbat4() {
+	if (b==0) {b=bat4.length-1}
+	else{b=(b-1)%bat4.length}
+	document.getElementById('bat').src=bat4[b]
+	/*pour aller vers la gauche*/
+}
+
+
+function changebat5() {
+	b=(b+1)%bat5.length
+	document.getElementById('bat').src=bat5[b]
+	/* pour aller vers la droite */
+	if(b==3){
+		document.getElementById("bateau7").hidden=false;
+		
+	}
+	else{
+		document.getElementById("bateau7").hidden=true;
+		
+	}
+}
+function changesbat5() {
+	if (b==0) {b=bat5.length-1}
+	else{b=(b-1)%bat5.length}
+	document.getElementById('bat').src=bat5[b]
+	if(b==3){
+		document.getElementById("bateau7").hidden=false;
+	}
+	else{
+		document.getElementById("bateau7").hidden=true;
+	}
+	/*pour aller vers la gauche*/
+}
+function changebat6() {
+	b=(b+1)%bat6.length
+	document.getElementById('bat').src=bat6[b]
+	/* pour aller vers la droite */
+}
+function changesbat6() {
+	if (b==0) {b=bat6.length-1}
+	else{b=(b-1)%bat6.length}
+	document.getElementById('bat').src=bat6[b]
+	/*pour aller vers la gauche*/
+}
+function eateregg(){
+	q=window.innerWidth;
+	d=window.innerHeight;
+	document.getElementById("bateau7").style="position:absolute;top:"+Math.random()*(d-140)+"px;left:"+Math.random()*(q-140)+"px;margin:0" ;
+}
+function fonctionwave(){
+	updateVan(0);
+	function updateVan(i)
+	{
+		var t=setTimeout(function(){
+			document.getElementById("imagewave").style.marginLeft=i+"px";
+			if(i<2000){updateVan(i+1);}
+			else{document.getElementById("imagewave").style="display:none;";}
+
+		},1);//vitesse 1
+	}
+
+}
+
